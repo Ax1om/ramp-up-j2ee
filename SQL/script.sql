b7605ee5cd13ddbb43cbb735c8b47717c5c3f66d
Mauricio Enriquez <mauricio.enriquez@nestle.com>
2017-08-24 18:37:38 +0200

    - Added SQL Script to populate database

---
+++ SQL/script.sql
@@ -0,0 +1,4 @@
+USE si2001_servlets;
+INSERT INTO marital_status (status) VALUES ("Married"), ("Single"), ("Divorced"), ("Widowed");
+INSERT INTO skill (name) VALUES ("Java"), ("HTML"), ("C"), ("C++"), ("JavaScript"), ("CSS");
+COMMIT;
